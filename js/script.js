8a3ae7cb3c4d465ac0f6eb18073c12e4a90f3618
MatteoArt <mattrim8@gmail.com>
2023-07-14 16:44:34 +0200

    completo milestone 0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 
 
-let teamMembers = 
+//creo l'array di oggetti
+let teamMembers = [
+    {
+        nameMember : "Wayne Barnett",
+        role : "Founder & CEO",
+        photo : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nameMember : "Angela Caroll",
+        role : "Chief Editor",
+        photo : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nameMember : "Walter Gordon",
+        role : "Office Manager",
+        photo : "walter-gordon-office-manager.jpg"
+    },
+    {
+        nameMember : "Angela Lopez",
+        role : "Social Media Manager",
+        photo : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nameMember : "Scott Estrada",
+        role : "Developer",
+        photo : "scott-estrada-developer.jpg"
+    },
+    {
+        nameMember : "Barbara Ramos",
+        role : "Graphic Designer",
+        photo : "barbara-ramos-graphic-designer.jpg"
+    }
+];
